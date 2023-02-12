483b3b8b9c4534bc3b5569f95a33c28bd2e56f39
Tvrtko <tvrtkobabic2@gmail.com>
2023-02-12 11:40:31 +0100

    update chessboard interface

--- src/components/Chessboard/interface.ts
+++ src/components/Chessboard/interface.ts
@@ -1,9 +1,19 @@
 export interface ChessboardProps {
   size?: string
   boardRef?: React.MutableRefObject<undefined>
+  fen?: string
   onMove?: (chess: OnMoveBoard) => Promise<void>
 }
 
 export interface OnMoveBoard {
   fen: () => string
+  pgn: (options?: PgnOptions) => string
+  moves: (options?: { verbose?: boolean }) => any
+  history: (options?: { verbose?: boolean }) => any
+}
+
+/** Example for html usage: .pgn({ max_width: 72, newline_char: "<br />" }) */
+export interface PgnOptions {
+  newline_char: string
+  max_width: number
 }
