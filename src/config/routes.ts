0f6dd8d79edbc18a48efacb17c16b54c1efd28ce
Tvrtko <tvrtkobabic2@gmail.com>
2023-04-12 22:03:24 +0200

    update routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,3 +1,12 @@
+const API_BASENAME = process.env.NEXT_PUBLIC_API_BASENAME
+
 export const routesMap = {
   public: ['/', '/login'],
 }
+
+export const puzzleSet = (id: string) => '/puzzles/set/' + id
+
+export const puzzleAPI = (puzzleId: string) => API_BASENAME + '/puzzle/' + puzzleId
+export const puzzleSetAPI = (setId: string) => API_BASENAME + '/sets/' + setId
+export const puzzleSetUpdateAPI = (setId: string, puzzleId: string) => API_BASENAME + '/sets/' + setId + '/' + puzzleId
+export const puzzleSetsAPI = (email: string) => API_BASENAME + '/sets/all/' + email
