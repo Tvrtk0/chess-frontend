946bca314bb1bc4b851b597e331a58c32b113920
Tvrtko <tvrtkobabic2@gmail.com>
2023-04-15 23:14:54 +0200

    create sets page

---
+++ pages/puzzles/index.tsx
@@ -0,0 +1,10 @@
+import PuzzleSets from 'modules/PuzzleSets'
+import React from 'react'
+
+export default function Puzzles() {
+  return (
+    <div className="container m-auto">
+      <PuzzleSets />
+    </div>
+  )
+}
---
+++ pages/puzzles/set/[id].tsx
@@ -0,0 +1,29 @@
+import PuzzleBoard from 'components/PuzzleBoard'
+import { routes } from 'config'
+import * as I from 'model'
+import FinishedSet from 'modules/FinishedSet'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import useSWR from 'swr'
+
+export default function Set() {
+  const router = useRouter()
+  const { id } = router.query
+
+  // @ts-ignore
+  const { data: set, mutate } = useSWR<I.Set>(routes.puzzleSetAPI(id))
+  const findNextPuzzle = () => set?.setPuzzles.find(p => p.played === false)
+  const [activePuzzleId, setActivePuzzleId] = useState(findNextPuzzle())
+
+  useEffect(() => {
+    setActivePuzzleId(set?.setPuzzles.find(p => p.played === false))
+  }, [set?.setPuzzles])
+
+  if (!set) return null
+  return (
+    <div className="container mx-auto mt-10">
+      {activePuzzleId && <PuzzleBoard setId={set._id} puzzleId={activePuzzleId.puzzleId} mutateSet={mutate} />}
+      {set?.setPuzzles.find(p => p.played === false) === undefined && <FinishedSet set={set} mutateSet={mutate} />}
+    </div>
+  )
+}
