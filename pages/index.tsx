5cbdb4ec3e4ab4064ccb4ce21260dd095a3c213b
Tvrtko <tvrtkobabic2@gmail.com>
2023-05-01 13:32:13 +0200

    create home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head'
+import HansTikkanenImage from '@/public/assets/img/HansTikkanen.jpg'
+import AxelSmith from '@/public/assets/img/AxelSmith.jpg'
+import Image from 'next/image'
+import { useMediaQuery } from 'utils/hooks/useMediaQuery'
 
 export default function Home() {
+  const isLg = useMediaQuery('lg')
+
   return (
     <>
       <Head>
@@ -10,11 +16,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <div className="container m-auto">
-          <div className="mt-40 flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="my-7 text-center lg:text-4xl">The Woodpecker Method</h1>
+        <section
+          className={`${isLg ? 'flex-row' : 'flex-col-reverse'} mt-10 flex items-center justify-center gap-x-32 px-5`}
+        >
+          <div className="flex max-w-lg flex-col justify-center p-3">
+            <div className="flex flex-col gap-y-3 text-lg leading-8">
+              <h2>Ultimate Method for Improvers</h2>
+              <p>
+                The Woodpecker Method is the name given by Axel Smith to a training system developed by his compatriot
+                and co-author Hans Tikkanen
+              </p>
+              <p>
+                After training with his method in 2010, Tikkanen achieved three GM norms within a seven-week period.
+              </p>
+            </div>
           </div>
-        </div>
+          <div className="my-5 flex max-w-sm flex-col items-center">
+            <Image src={AxelSmith} alt="Axel Smith" width={420} height={420} />
+            <p className="mt-2 italic text-stone-400">GM Axel Smith</p>
+          </div>
+        </section>
+
+        <section
+          className={`${
+            isLg ? 'flex-row' : 'flex-col'
+          } mt-12 flex items-center justify-center gap-x-32 bg-stone-800 py-14 px-5`}
+        >
+          <div className="my-5 flex max-w-sm flex-col items-center">
+            <Image src={HansTikkanenImage} alt="Hans Tikkanen" width={420} height={420} />
+            <p className="mt-2 italic text-stone-400">GM Hans Tikkanen</p>
+          </div>
+          <div className="flex max-w-lg flex-col justify-center gap-y-5 text-lg leading-8">
+            <h2>No shortcuts for success. Progress requires hard work!</h2>
+            <p>
+              The quick explanation of the Woodpecker Method is that you need to solve a large number of puzzles in a
+              row; then solve the same puzzles again and again, only faster.
+            </p>
+            <p>
+              It&#39;s not a lazy shortcut to success – hard work is required. But the reward can be re-programming your
+              unconscious mind. Benefits include sharper tactical vision, fewer blunders, better play when in time
+              trouble and improved intuition.
+            </p>
+          </div>
+        </section>
       </div>
     </>
   )
